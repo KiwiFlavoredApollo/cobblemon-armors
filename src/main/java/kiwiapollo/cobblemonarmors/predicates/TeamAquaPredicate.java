408c0c9104496ddf6651dc586bafe0214320bd4e
fractalbarista <fractalbarista@gmail.com>
2024-08-29 11:23:22 +0900

    edited TeamAquaPredicate blacklisting certain type of Pokemons

--- src/main/java/kiwiapollo/cobblemonarmors/predicates/TeamAquaPredicate.java
+++ src/main/java/kiwiapollo/cobblemonarmors/predicates/TeamAquaPredicate.java
@@ -1,13 +1,31 @@
 package kiwiapollo.cobblemonarmors.predicates;
 
+import com.cobblemon.mod.common.api.storage.party.PlayerPartyStore;
+import com.cobblemon.mod.common.api.types.ElementalType;
+import com.cobblemon.mod.common.api.types.ElementalTypes;
+import com.cobblemon.mod.common.pokemon.Pokemon;
 import net.minecraft.server.network.ServerPlayerEntity;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.function.Predicate;
+import java.util.stream.Stream;
+import java.util.stream.StreamSupport;
 
 public class TeamAquaPredicate implements Predicate<ServerPlayerEntity> {
+    private static final List<ElementalType> BLACKLISTED_TYPES = List.of(
+            ElementalTypes.INSTANCE.getFIRE()
+    );
 
     @Override
     public boolean test(ServerPlayerEntity player) {
-        return false;
+        for (Pokemon pokemon : new PlayerPartyStore(player.getUuid())) {
+            Stream<ElementalType> types = StreamSupport.stream(pokemon.getTypes().spliterator(), false);
+            if (types.anyMatch(BLACKLISTED_TYPES::contains)) {
+                return false;
+            }
+        }
+
+        return true;
     }
 }
