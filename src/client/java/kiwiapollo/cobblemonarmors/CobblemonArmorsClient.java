99bd34646a83bc7a84af4306ac8aa0384e4c1e53
fractalbarista <fractalbarista@gmail.com>
2024-08-29 11:24:43 +0900

    added ClientRenderProvider instantiation, fixing crash

--- src/client/java/kiwiapollo/cobblemonarmors/CobblemonArmorsClient.java
+++ src/client/java/kiwiapollo/cobblemonarmors/CobblemonArmorsClient.java
@@ -5,6 +5,8 @@ import net.fabricmc.api.ClientModInitializer;
 import software.bernie.geckolib.animatable.client.RenderProvider;
 
 public class CobblemonArmorsClient implements ClientModInitializer {
+	public static final RenderProvider RENDER_PROVIDER = new ClientRenderProvider();
+
 	@Override
 	public void onInitializeClient() {
 		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
