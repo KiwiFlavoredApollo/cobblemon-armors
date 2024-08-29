2a7041bc1278b7bb95e9f367b3b1e37ce7fca44f
fractalbarista <fractalbarista@gmail.com>
2024-08-29 11:23:19 +0900

    removed whitespace

--- src/main/java/kiwiapollo/cobblemonarmors/armors/TeamMagmaArmorSet.java
+++ src/main/java/kiwiapollo/cobblemonarmors/armors/TeamMagmaArmorSet.java
@@ -9,7 +9,7 @@ public class TeamMagmaArmorSet extends ArmorSet {
                 new Armor(ArmorMaterials.TEAM_MAGMA, ArmorItem.Type.HELMET, "team_magma_helmet"),
                 new Armor(ArmorMaterials.TEAM_MAGMA, ArmorItem.Type.CHESTPLATE, "team_magma_chestplate"),
                 new Armor(ArmorMaterials.TEAM_MAGMA, ArmorItem.Type.LEGGINGS, "team_magma_leggings"),
-                new Armor(ArmorMaterials.TEAM_MAGMA, ArmorItem.Type.BOOTS, "team_magma_boots ")
+                new Armor(ArmorMaterials.TEAM_MAGMA, ArmorItem.Type.BOOTS, "team_magma_boots")
         );
     }
 }
