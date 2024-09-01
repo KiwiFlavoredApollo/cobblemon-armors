fd182cdb10d315d39d8fa1cf52072341a3bdef70
fractalbarista <fractalbarista@gmail.com>
2024-09-01 21:27:05 +0900

    added Test Armors

--- src/client/java/kiwiapollo/cobblemonarmors/CobblemonArmorsClient.java
+++ src/client/java/kiwiapollo/cobblemonarmors/CobblemonArmorsClient.java
@@ -2,11 +2,14 @@ package kiwiapollo.cobblemonarmors;
 
 import kiwiapollo.cobblemonarmors.render.MythrilArmorRenderProviderFactoryClient;
 import kiwiapollo.cobblemonarmors.render.MythrilArmorRenderProviderFactoryProxy;
+import kiwiapollo.cobblemonarmors.render.TestArmorRenderProviderFactoryClient;
+import kiwiapollo.cobblemonarmors.render.TestArmorRenderProviderFactoryProxy;
 import net.fabricmc.api.ClientModInitializer;
 
 public class CobblemonArmorsClient implements ClientModInitializer {
 	@Override
 	public void onInitializeClient() {
 		MythrilArmorRenderProviderFactoryProxy.setRenderProviderFactory(new MythrilArmorRenderProviderFactoryClient());
+		TestArmorRenderProviderFactoryProxy.setRenderProviderFactory(new TestArmorRenderProviderFactoryClient());
 	}
 }
---
+++ src/client/java/kiwiapollo/cobblemonarmors/render/TestArmorRenderProviderFactoryClient.java
@@ -0,0 +1,32 @@
+package kiwiapollo.cobblemonarmors.render;
+
+import kiwiapollo.cobblemonarmors.CobblemonArmors;
+import kiwiapollo.cobblemonarmors.armors.MythrilArmorItem;
+import net.minecraft.client.render.entity.model.BipedEntityModel;
+import net.minecraft.entity.EquipmentSlot;
+import net.minecraft.entity.LivingEntity;
+import net.minecraft.item.ItemStack;
+import net.minecraft.util.Identifier;
+import software.bernie.geckolib.animatable.client.RenderProvider;
+import software.bernie.geckolib.model.DefaultedItemGeoModel;
+import software.bernie.geckolib.renderer.GeoArmorRenderer;
+
+public class TestArmorRenderProviderFactoryClient implements RenderProviderFactory {
+    @Override
+    public RenderProvider create() {
+        return new RenderProvider() {
+            @Override
+            public BipedEntityModel<LivingEntity> getHumanoidArmorModel(
+                    LivingEntity livingEntity, ItemStack itemStack,
+                    EquipmentSlot equipmentSlot, BipedEntityModel<LivingEntity> original
+            ) {
+                GeoArmorRenderer<MythrilArmorItem> renderer = new GeoArmorRenderer<>(
+                        new DefaultedItemGeoModel<>(Identifier.of(CobblemonArmors.NAMESPACE, "test_armor"))
+                );
+                renderer.prepForRender(livingEntity, itemStack, equipmentSlot, original);
+
+                return renderer;
+            }
+        };
+    }
+}
