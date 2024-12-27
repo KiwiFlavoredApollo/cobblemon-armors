4e2c09d1b18cda7a5fe11d2100c3d920d2743cc1
fractalbarista <fractalbarista@gmail.com>
2024-12-27 12:02:59 +0900

    Added DataGenerator

---
+++ src/main/java/kiwiapollo/cobblemonarmors/datagen/DataGenerator.java
@@ -0,0 +1,46 @@
+package kiwiapollo.cobblemonarmors.datagen;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataOutput;
+import net.fabricmc.fabric.api.datagen.v1.provider.FabricRecipeProvider;
+import net.fabricmc.fabric.api.datagen.v1.provider.FabricTagProvider;
+import net.minecraft.data.server.recipe.RecipeJsonProvider;
+import net.minecraft.item.Item;
+import net.minecraft.registry.RegistryKeys;
+import net.minecraft.registry.RegistryWrapper;
+
+import java.util.concurrent.CompletableFuture;
+import java.util.function.Consumer;
+
+public class DataGenerator implements DataGeneratorEntrypoint {
+    @Override
+    public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+        FabricDataGenerator.Pack pack = fabricDataGenerator.createPack();
+
+        pack.addProvider(RecipeProvider::new);
+        pack.addProvider(TagProvider::new);
+    }
+
+    private static class RecipeProvider extends FabricRecipeProvider {
+        public RecipeProvider(FabricDataOutput output) {
+            super(output);
+        }
+
+        @Override
+        public void generate(Consumer<RecipeJsonProvider> exporter) {
+
+        }
+    }
+
+    private static class TagProvider extends FabricTagProvider<Item> {
+        public TagProvider(FabricDataOutput output, CompletableFuture<RegistryWrapper.WrapperLookup> registriesFuture) {
+            super(output, RegistryKeys.ITEM, registriesFuture);
+        }
+
+        @Override
+        protected void configure(RegistryWrapper.WrapperLookup arg) {
+
+        }
+    }
+}
