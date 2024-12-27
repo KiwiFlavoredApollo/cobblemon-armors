b5d60da70485911c9730f1c4f95341f9bede3148
fractalbarista <fractalbarista@gmail.com>
2024-12-27 17:31:51 +0900

    Edited to use Identifier.of() instead of Identifier constructor

--- src/main/java/kiwiapollo/cobblemonarmors/CobblemonArmors.java
+++ src/main/java/kiwiapollo/cobblemonarmors/CobblemonArmors.java
@@ -26,7 +26,7 @@ public class CobblemonArmors implements ModInitializer {
 	public static final Logger LOGGER = LoggerFactory.getLogger(NAMESPACE);
 
 	public static final RegistryKey<ItemGroup> COBBLEMON_ARMOR_ITEM_GROUP_KEY =
-			RegistryKey.of(Registries.ITEM_GROUP.getKey(), new Identifier(NAMESPACE, "item_group"));
+			RegistryKey.of(Registries.ITEM_GROUP.getKey(), Identifier.of(NAMESPACE, "item_group"));
 	public static final ItemGroup COBBLEMON_ARMOR_ITEM_GROUP = FabricItemGroup.builder()
 			.icon(() -> new ItemStack(Registries.ITEM.get(Identifier.of(NAMESPACE, "fennekin_chestplate"))))
 			.displayName(Text.translatable("Cobblemon Armors"))
