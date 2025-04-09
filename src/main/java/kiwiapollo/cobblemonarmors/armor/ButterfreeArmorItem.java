d2edc4d1a813d853ade53126a309146e32f9afe9
fractalbarista <fractalbarista@gmail.com>
2025-04-09 16:23:34 +0900

    Added assets for Butterfree items and armors

---
+++ src/main/java/kiwiapollo/cobblemonarmors/armor/ButterfreeArmorItem.java
@@ -0,0 +1,9 @@
+package kiwiapollo.cobblemonarmors.armor;
+
+import net.minecraft.item.ArmorMaterial;
+
+public class ButterfreeArmorItem extends CustomModelArmorItem {
+    public ButterfreeArmorItem(ArmorMaterial material, Type type, Settings settings) {
+        super(material, type, settings, "butterfree_armor");
+    }
+}
