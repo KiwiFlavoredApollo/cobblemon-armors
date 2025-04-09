93a09c666a47444218df5163a077bdb938f29b18
fractalbarista <fractalbarista@gmail.com>
2025-04-09 16:24:07 +0900

    Added assets for Dawn's helmet items and armors

---
+++ src/main/java/kiwiapollo/cobblemonarmors/armor/DawnArmorItem.java
@@ -0,0 +1,9 @@
+package kiwiapollo.cobblemonarmors.armor;
+
+import net.minecraft.item.ArmorMaterial;
+
+public class DawnArmorItem extends CustomModelArmorItem {
+    public DawnArmorItem(ArmorMaterial material, Type type, Settings settings) {
+        super(material, type, settings, "dawn_armor");
+    }
+}
