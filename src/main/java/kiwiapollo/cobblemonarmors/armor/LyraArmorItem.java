554c7aae0fd0228d5e044b1fe2c3aa6cea6037b2
fractalbarista <fractalbarista@gmail.com>
2025-04-09 16:53:27 +0900

    Added Lyra armors

---
+++ src/main/java/kiwiapollo/cobblemonarmors/armor/LyraArmorItem.java
@@ -0,0 +1,9 @@
+package kiwiapollo.cobblemonarmors.armor;
+
+import net.minecraft.item.ArmorMaterial;
+
+public class LyraArmorItem extends CustomModelArmorItem {
+    public LyraArmorItem(ArmorMaterial material, Type type, Settings settings) {
+        super(material, type, settings, "lyra_armor");
+    }
+}
