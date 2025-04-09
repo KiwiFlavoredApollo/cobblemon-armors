7fdb622f6610ac5b1e251e6e75442fbcef86a246
fractalbarista <fractalbarista@gmail.com>
2025-04-09 16:36:12 +0900

    Added Slowking armors

---
+++ src/main/java/kiwiapollo/cobblemonarmors/armor/SlowkingArmorItem.java
@@ -0,0 +1,9 @@
+package kiwiapollo.cobblemonarmors.armor;
+
+import net.minecraft.item.ArmorMaterial;
+
+public class SlowkingArmorItem extends CustomModelArmorItem {
+    public SlowkingArmorItem(ArmorMaterial material, Type type, Settings settings) {
+        super(material, type, settings, "slowking_armor");
+    }
+}
