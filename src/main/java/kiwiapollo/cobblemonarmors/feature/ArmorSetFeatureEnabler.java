57c2dc62fd86282948f5a8cdcbcb1ed916651a15
fractalbarista <fractalbarista@gmail.com>
2025-10-27 20:28:58 +0900

    Renamed game rule to enableCobblemonArmorsFeature Edited English translation for enableCobblemonArmorsFeature game rule

--- src/main/java/kiwiapollo/cobblemonarmors/feature/ArmorSetFeatureEnabler.java
+++ src/main/java/kiwiapollo/cobblemonarmors/feature/ArmorSetFeatureEnabler.java
@@ -31,7 +31,7 @@ public abstract class ArmorSetFeatureEnabler implements ServerTickEvents.EndTick
 
     @Override
     public void onEndTick(MinecraftServer server) {
-        if (!server.getGameRules().getBoolean(ModGameRule.ENABLE_COBBLEMON_ARMOR_SET_FEATURES)) {
+        if (!server.getGameRules().getBoolean(ModGameRule.ENABLE_COBBLEMON_ARMORS_FEATURE)) {
             return;
         }
 
--- src/main/java/kiwiapollo/cobblemonarmors/gamerule/ModGameRule.java
+++ src/main/java/kiwiapollo/cobblemonarmors/gamerule/ModGameRule.java
@@ -5,9 +5,9 @@ import net.fabricmc.fabric.api.gamerule.v1.GameRuleRegistry;
 import net.minecraft.world.GameRules;
 
 public class ModGameRule {
-    public static GameRules.Key<GameRules.BooleanRule> ENABLE_COBBLEMON_ARMOR_SET_FEATURES;
+    public static GameRules.Key<GameRules.BooleanRule> ENABLE_COBBLEMON_ARMORS_FEATURE;
 
     public static void register() {
-        ENABLE_COBBLEMON_ARMOR_SET_FEATURES = GameRuleRegistry.register("enableCobblemonArmorSetFeatures", GameRules.Category.MISC, GameRuleFactory.createBooleanRule(false));
+        ENABLE_COBBLEMON_ARMORS_FEATURE = GameRuleRegistry.register("enableCobblemonArmorsFeature", GameRules.Category.MISC, GameRuleFactory.createBooleanRule(false));
     }
 }
